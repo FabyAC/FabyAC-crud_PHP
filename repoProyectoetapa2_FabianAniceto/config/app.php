f392f4408be6c0722045ca907597582bbb659444
FabyAC <fhac2006@gmail.com>
2024-11-25 02:42:45 -0300

    feature

---
+++ repoProyectoetapa2_FabianAniceto/config/app.php
@@ -0,0 +1,7 @@
+<?php
+    
+    const APP_URL="http://localhost/repoProyectoetapa2_FabianAniceto/";
+    const APP_NAME="CRUD POO y MySQL";
+    const APP_SESSION_NAME="CRUD";
+    date_default_timezone_set("America/Argentina/Buenos_Aires");
+?>
